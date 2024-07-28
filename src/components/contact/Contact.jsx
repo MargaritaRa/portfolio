c458648f62f830ce17d7b7ce723ce03eaa3be3b6
Margarita Ramirez <margarita26.mr@gmail.com>
2024-07-28 14:44:46 -0400

    contact complete

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,6 +3,11 @@ import CG from '../../../public/image/CG.png'
 import synchrony from '../../../public/image/synchrony.png'
 import ocps from '../../../public/image/ocps.png'
 import contiki from '../../../public/image/contiki.png'
+import yout from '../../../public/image/yout.png'
+import insta from '../../../public/image/insta.png'
+import tweet from '../../../public/image/tweet.png'
+import face from '../../../public/image/face.png'
+
 
 
 function Contact(){
@@ -20,7 +25,21 @@ function Contact(){
                 </div>
             </div>
             <div id="contact">
-
+                <h1 className="contactPageTitle">Contact Me</h1>
+                <span className="contactDesc">Please fill out the form below to discuss any work oppertunities.
+                </span>
+                <form action="" className="contactForm">
+                    <input type="text" className="name" placeholder='Your Name'/>
+                    <input type="email" className="email" placeholder='Your Email' />
+                    <textarea name="message" rows="5" placeholder='Your message' className='msg'></textarea>
+                    <button type='submit' value='Send' className="submitBtn">Submit</button>
+                    <div className="links">
+                        <img src={face} alt="Facebook" className="link" />
+                        <img src={yout} alt="Youtube" className="link" />
+                        <img src={insta} alt="Instagram" className="link" />
+                        <img src={tweet} alt="Twitter" className="link" />
+                    </div>
+                </form>
             </div>
         </section>
     );
